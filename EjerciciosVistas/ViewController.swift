4b14609f8bee43d4ec54b7c39af0cab901974d81
di-saldana <dsl42@alu.ua.es>
2023-11-12 20:23:55 +0100

    Campo de texto

--- EjerciciosVistas/ViewController.swift
+++ EjerciciosVistas/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var textField: UITextField!
@@ -15,9 +15,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        textField.delegate = self
+        
+        self.textView.backgroundColor = .cyan
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with: UIEvent?) {
+       print("¡touch en la pantalla!")
+       //Necesitamos un outlet para acceder al campo de texto.
+       //Aquí suponemos que lo hemos llamado "campoDeTexto"
+       self.textField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {   //delegate method
+        self.textField.resignFirstResponder()
+      return true
     }
 
     @IBAction func introPulsado(_ sender: UITextField) {
+        if let text = sender.text, !text.isEmpty {
+            self.textView.text.append("\(text)\n")
+        }
+
+        sender.text = ""
         sender.resignFirstResponder()
     }
     
